b51b2b7a75454e035ab3b41daeb583c76b69a71d
Randy Deng <ryangdeng@gmail.com>
2019-07-02 13:46:18 -0400

    cleaned up code and added some checks for win condition

--- connect4bot/qnet/gym_connect4/envs/connect4.py
+++ connect4bot/qnet/gym_connect4/envs/connect4.py
@@ -5,13 +5,18 @@ import numpy as np
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+
+HEIGHT = 6
+WIDTH = 7
+
+
 class C4Env(gym.Env):
     metadata = {'render.modes': ['human', 'bot']}
 
     def __init__(self):
         # 6 x 7 board size
         self._reset()
-        self.player = Locations.P1
+        self.player = Locations.P1.value
         #self.opponent = Locations.P2
         pass
 
@@ -25,7 +30,7 @@ class C4Env(gym.Env):
         #self.done, tie = self.check_win_condition()
         #if self.done:
          #   return self.state, reward, self.done, {'state': self.state}
-        self.perform_move(action, 1)
+        self.perform_move(action, Locations.P1.value)
         return self.state, 0, self.done, {'state': self.state}
         pass
 
@@ -33,32 +38,42 @@ class C4Env(gym.Env):
         return
 
     def _reset(self):
-        self.board = np.zeros(6 * 7)
+        self.board = np.zeros(HEIGHT * WIDTH).reshape(HEIGHT, WIDTH)
         self.state = {'board': self.board}
         self.done = False
-        self.current_player = Locations.P1
-        self.action_space = spaces.Discrete(7)
+        self.current_player = Locations.P1.value
+        self.action_space = spaces.Discrete(WIDTH)
         pass
 
     def _render(self, mode='human', close=False):
-        print(self.board.reshape(6,7))
+        print(self.board)
         print("")
-        pass
 
     def check_win_condition(self):
-        if Locations.Empty in self.board:
-            pass
-        pass
+        # horizontalCheck
+        for j in range(WIDTH - 3):
+            for i in range(HEIGHT):
+                if (self.board[i][j] == Locations.P1.value and
+                    self.board[i][j+1] == Locations.P1.value and
+                    self.board[i][j+2] == Locations.P1.value and
+                    self.board[i][j+3] == Locations.P1.value):
+                        return True
+
+        # verticalCheck
+        for i in range(HEIGHT - 3):
+            for j in range(WIDTH):
+                if (self.board[i][j] == Locations.P1.value and
+                    self.board[i+1][j] == Locations.P1.value and
+                    self.board[i+2][j] == Locations.P1.value and
+                    self.board[i+3][j] == Locations.P1.value):
+                        return True
+
     
     def perform_move(self, action, player_num):
-        """
-        Assumes that the move is legal
-        """
-        temp = self.board.reshape(6,7)
-        chosen_col = temp[:,(action-1)]
+        """ Assumes that the move is legal """
+        chosen_col = self.board[:, (action - 1)]
         for i in range(len(chosen_col)):
-            #breakpoint()
-            if int(chosen_col[6 - i -1]) == 0:
+            if int(chosen_col[6 - i - 1]) == 0:
                 chosen_col[6 - i - 1] = player_num
                 break
 

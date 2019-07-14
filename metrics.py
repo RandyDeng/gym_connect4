536a92d7952cc5a3eab635b86bc91949b521dd2a
Randy Deng <ryangdeng@gmail.com>
2019-07-14 13:59:31 -0400

    Added metrics images

--- metrics.py
+++ metrics.py
@@ -3,10 +3,20 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    filename = 'dqn_MaxBoltzmann{}'
-    data = pickle.load(open(filename.format('.metrics'), 'rb'))
-    # loss', 'mean_absolute_error', 'mean_q']
-    plt.plot(data['loss'])
+    filename = 'max_boltzmann_run/dqn_MaxBoltzmann.metrics'
+    filename = 'eps_greedy_run/dqn_EpsGreedy{}.metrics'
+    filtered_data = []
+    for i in range(10):
+        data = pickle.load(open(filename.format(i), 'rb'))['mean_absolute_error']
+        batch_size = 2000
+        for j in range(int(len(data) / batch_size)):
+            batch = data[j * batch_size : j * batch_size + batch_size]
+            filtered_data.append(sum(batch) / batch_size)
+    # 'loss', 'mean_absolute_error', 'mean_q']
+    plt.plot(filtered_data)
+    plt.title('Epsilon Greedy Mean Absolute Error')
+    plt.ylabel('Average Mean Absolute Error in Batches of 2000')
+    plt.xlabel('Steps in Batches of 2000')
     plt.show()
 
 

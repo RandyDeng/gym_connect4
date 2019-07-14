5dfc421979be5fa939c72474a1e2b728cb8c42ac
LookItsJosh <josh@JoshDesktop.localdomain>
2019-07-14 12:11:30 -0400

    Testing

--- train.py
+++ train.py
@@ -52,14 +52,14 @@ def main():
     # Available environments:
     # 'gym_connect4:Connect4VsRandomBot-v0'
     # 'gym_connect4:Connect4VsSelf-v0'
-    # 'gym_connect4:Connect4VsHuman-v0'
+    #env_name = 'gym_connect4:Connect4VsHuman-v0'
     env_name = 'gym_connect4:Connect4VsRandomBot-v0'
     env = gym.make(env_name)
 
     # Build Neural Network Architecture for Reinforcement Learning
     model = build_model()
     memory = SequentialMemory(limit=1000000, window_length=1)
-    policy = MaxBoltzmannQPolicy()
+    policy = EpsGreedyQPolicy()
     dqn = DQNAgent(model=model,
                    nb_actions=7,
                    memory=memory,
@@ -69,16 +69,17 @@ def main():
     metrics = Metrics(dqn)
 
     # Train the Neural Network
-    filename = 'dqn_MaxBoltzmann{}'
+    filename = 'dqn_EpsGreedy{}'
     dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-    # dqn.load_weights(filename.format('.weights32'))
-    dqn.fit(env, nb_steps=2000000, visualize=False, verbose=2, callbacks=[metrics])
+    dqn.load_weights(filename.format('.weights32'))
+    dqn.fit(env, nb_steps=1000000, visualize=False, verbose=2, callbacks=[metrics])
     dqn.save_weights(filename.format('.weights32'), overwrite=True)
-    dqn.test(env, nb_episodes=100, visualize=False)
+    pickle.dump(metrics.metrics, open(filename.format('.metrics'), 'wb'))
+    #dqn.load_weights(filename.format('.weights32'))
+    dqn.test(env, nb_episodes=100, visualize=True)
     env.close()
     
     # Save metrics to file
-    pickle.dump(metrics.metrics, open(filename.format('.metrics'), 'wb'))
 
 if __name__ == "__main__":
     main()
